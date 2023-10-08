b120521a33df2318192326798bbd0c5824e89fc3
zyrouge <zyrouge@hotmail.com>
2023-10-08 16:10:20 +0530

    refactor: create needed dirs

--- scripts/build-podman.sh
+++ scripts/build-podman.sh
@@ -11,17 +11,28 @@ app_version=$1
 container_name="android-studio-preview-appimage"
 work_dir="/android-studio-preview-appimage"
 
+echo "Starting container..."
 podman run --rm -dti --name "${container_name}" ubuntu:20.04
 podman wait --condition=running "${container_name}"
+
+echo "Installing dependencies..."
 podman exec "${container_name}" apt update
 podman exec "${container_name}" apt-get install -y curl desktop-file-utils
+
+echo "Copying necessities..."
 podman exec "${container_name}" mkdir "${work_dir}"
 for x in "scripts" "templates"; do
     podman cp "./${x}" "${container_name}:${work_dir}/${x}"
 done
 podman exec "${container_name}" find "${work_dir}/scripts" -type f -name "*.sh" -exec chmod +x {} \;
+
+echo "Starting build..."
 podman exec "${container_name}" "${work_dir}/scripts/build.sh" "${app_version}"
+
+echo "Copying build artifacts..."
 podman cp "${container_name}:${work_dir}/dist" "${dist_dir}"
+
+echo "Stopping container..."
 podman stop "${container_name}"
 
 echo "Done!"
--- scripts/build.sh
+++ scripts/build.sh
@@ -17,17 +17,18 @@ echo "Android Studio Version: ${app_version}"
 app_title="Android Studio"
 app_name="android-studio"
 
-appimage_tool="${artifacts_dir}/appimagetool.AppImage"
-appimage_tool_url="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+appimagetool="${artifacts_dir}/appimagetool.AppImage"
+appimagetool_url="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
 
-if ! [ -f "${appimage_tool}" ]; then
-    echo "Downloading ${appimage_tool_url}"
-    curl -Ls -A "${curl_ua}" "${appimage_tool_url}" -o "${appimage_tool}"
-    echo "Downloaded ${appimage_tool}"
+mkdir -p "${artifacts_dir}"
+if ! [ -f "${appimagetool}" ]; then
+    echo "Downloading ${appimagetool_url}"
+    curl -Ls -A "${curl_ua}" "${appimagetool_url}" -o "${appimagetool}"
+    echo "Downloaded ${appimagetool}"
 else
     echo "Skipping AppImageTool..."
 fi
-chmod +x "${appimage_tool}"
+chmod +x "${appimagetool}"
 
 app_dir="${artifacts_dir}/${app_name}.AppDir"
 archive_file="${artifacts_dir}/android-studio-${app_version}-linux.tar.gz"
@@ -53,7 +54,6 @@ fi
 d_icon="${app_dir}/bin/studio.png"
 d_icon_vector="${app_dir}/bin/studio.svg"
 
-# prepare AppDir
 cp "${d_icon}" "${app_dir}/${app_name}.png"
 cp "${d_icon_vector}" "${app_dir}/${app_name}.svg"
 convert "${d_icon}" -resize 256x256 "${app_dir}/.DirIcon"
@@ -74,6 +74,6 @@ appimage_file="${dist_dir}/${app_name}-${app_version}-${appimage_arch}.AppImage"
 
 echo "Building ${app_dir}"
 mkdir -p "${dist_dir}"
-ARCH=$appimage_arch "${appimage_tool}" "${app_dir}" "${appimage_file}"
+ARCH=$appimage_arch "${appimagetool}" "${app_dir}" "${appimage_file}"
 chmod +x "${appimage_file}"
 echo "Created ${appimage_file}"
