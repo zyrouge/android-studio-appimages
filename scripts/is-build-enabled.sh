6ecc18a2bd698a4ec631b20db3b835511b129bbb
zyrouge <hello@zyrouge.me>
2024-04-18 14:38:49 +0530

    ci: fix potential never build

--- scripts/is-build-enabled.sh
+++ scripts/is-build-enabled.sh
@@ -4,11 +4,11 @@ set -eu
 
 result="no"
 
-if [ "${X_APP_RELEASE}" == "stable" ] && [ "${X_BUILD_STABLE}" == "true" ]; then
+if [ "${X_APP_RELEASE}" == "stable" ] && [ "${X_BUILD_STABLE}" != "false" ]; then
     result="yes"
 fi
 
-if [ "${X_APP_RELEASE}" == "beta" ] && [ "${X_BUILD_BETA}" == "true" ]; then
+if [ "${X_APP_RELEASE}" == "beta" ] && [ "${X_BUILD_BETA}" != "false" ]; then
     result="yes"
 fi
 
